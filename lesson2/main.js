d6c485b90099b385641341d7ece941acf2fa472d
sae-github <miyasae1126@gmail.com>
2021-08-02 06:26:04 +0900

    refactor:main.js

--- lesson2/main.js
+++ lesson2/main.js
@@ -1,16 +1,15 @@
 const lists = document.getElementById('js-lists');
 const list = document.createElement('li');
-const text = document.createTextNode('これです');
 
 const anchor = document.createElement('a');
 anchor.href = '1.html';
+anchor.textContent = "これです";
 
 const image = document.createElement('img');
 image.src = 'bookmark.png';
 image.alt = 'ブックマーク';
 
-lists.appendChild(list);
-list.appendChild(anchor);
-anchor.appendChild(image);
-anchor.appendChild(text);
-
+lists
+.appendChild(list)
+.appendChild(anchor)
+.insertAdjacentElement('afterbegin',image);
