7a0e768bbc6b5cb0e353d9d6ffa462e8e26a6d6e
sae-github <miyasae1126@gmail.com>
2021-08-07 11:27:26 +0900

    feat: create lesson3

---
+++ lesson3/main.js
@@ -0,0 +1,6 @@
+const lists = document.getElementById('js-lists');
+
+const frag = document.createDocumentFragment();
+const list = document.createElement('li');
+const a = document.createElement('a');
+const image = document.createElement('img');
