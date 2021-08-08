4c7d68684a103b9088686e6fcd7c4074f1a1a1de
sae-github <miyasae1126@gmail.com>
2021-08-08 10:59:01 +0900

    feat:index.html main.js

--- lesson3/main.js
+++ lesson3/main.js
@@ -1,6 +1,28 @@
-const lists = document.getElementById('js-lists');
 
+const ul = document.getElementById('js-ul');
 const frag = document.createDocumentFragment();
-const list = document.createElement('li');
-const a = document.createElement('a');
-const image = document.createElement('img');
+
+const items = [
+  { href: "a1.html", src:"/img/bookmark.png",text: "a1"},
+  { href: "a2.html", src:"/img/message.png",text: "a2"}
+]
+
+for(let i = 0, len = items.length; i < len; i++) {
+  const item = items[i];
+  const li = document.createElement('li');
+  const anchor = document.createElement('a');
+  const image = document.createElement('img');
+  
+  anchor.href = item.href;
+  image.src = item.src;
+
+
+  li
+  .appendChild(anchor)
+  .appendChild(image);
+
+  anchor.insertAdjacentHTML('beforeend', item.text);
+  frag.appendChild(li);
+} 
+
+ul.appendChild(frag);
