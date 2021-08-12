101b52c8781fb43c51585e385251ad0d6a299778
sae-github <miyasae1126@gmail.com>
2021-08-12 20:24:22 +0900

    feat: Change the argument name

--- lesson5/main.js
+++ lesson5/main.js
@@ -1,28 +1,29 @@
 const ul = document.getElementById('js-ul');
 
-const items = [
+const lists = [
   { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
   { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" }
 ]
 
-new Promise((resolve) => {
-  resolve(items);
-}).then((items) => {
-  items.forEach(item => {
+const promise = new Promise((resolve) => {
+  resolve(lists);
+}).then((lists) => {
+  lists.forEach(list => {
     const frag = document.createDocumentFragment();
     const li = document.createElement('li');
     const anchor = document.createElement('a');
     const img = document.createElement('img');
 
-    anchor.href = item.to;
-    img.src = item.img;
-    img.alt = item.alt;
+    anchor.href = list.to;
+    img.src = list.img;
+    img.alt = list.alt;
 
     li.appendChild(anchor).appendChild(img);
-    anchor.insertAdjacentHTML('beforeend', item.text)
+    anchor.insertAdjacentHTML('beforeend', list.text)
     frag.appendChild(li)
     ul.appendChild(frag);
   });
-})
+});
+
 
 
