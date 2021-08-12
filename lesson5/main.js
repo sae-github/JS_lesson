6ee539d88e142e18304b47ebd0f29fd3d653d46a
sae-github <miyasae1126@gmail.com>
2021-08-12 22:40:12 +0900

    fix:main.js
    Extract DocumentFragment from forEach
    
    Remove unnecessary variable definitions

--- lesson5/main.js
+++ lesson5/main.js
@@ -1,28 +1,28 @@
+
 const ul = document.getElementById('js-ul');
+const frag = document.createDocumentFragment();
 
 const lists = [
   { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
   { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" }
 ]
 
-const promise = new Promise((resolve) => {
+new Promise((resolve) => {
   resolve(lists);
 }).then((lists) => {
   lists.forEach(list => {
-    const frag = document.createDocumentFragment();
     const li = document.createElement('li');
     const anchor = document.createElement('a');
     const img = document.createElement('img');
-    
+
     anchor.href = `/${list.to}`;
     img.src = list.img;
     img.alt = list.alt;
 
-    li.appendChild(anchor).appendChild(img);
-    anchor.insertAdjacentHTML('beforeend', list.text)
-    frag.appendChild(li)
-    ul.appendChild(frag);
+    frag.appendChild(li).appendChild(anchor).appendChild(img);
+    anchor.insertAdjacentHTML('beforeend',list.text);
   });
+  ul.appendChild(frag);
 });
 
 
