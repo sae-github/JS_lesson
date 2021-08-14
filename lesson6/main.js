83612a9fee2a32a9e8842a575fc569d10d470446
sae-github <miyasae1126@gmail.com>
2021-08-14 16:08:36 +0900

    fix: main.js

--- lesson6/main.js
+++ lesson6/main.js
@@ -1,31 +1,32 @@
-const ul = document.getElementById('js-ul');
-const frag = document.createDocumentFragment();
 
 const lists = [
-  {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"}, 
-  {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+  { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
+  { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" }
 ]
 
 function getListsObj() {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(lists);
-    },3000);
+    }, 3000);
   });
 }
 
-getListsObj().then((lists)=> {
-  lists.forEach(list => {
+getListsObj().then((date) => {
+  const ul = document.getElementById('js-ul');
+  const frag = document.createDocumentFragment();
+
+  date.forEach(value => {
     const li = document.createElement('li');
     const anchor = document.createElement('a');
     const img = document.createElement('img');
 
-    anchor.href = `/${list.to}`;
-    img.src = list.img;
-    img.alt = list.alt;
+    anchor.href = `/${value.to}`;
+    img.src = value.img;
+    img.alt = value.alt;
 
     frag.appendChild(li).appendChild(anchor).appendChild(img);
-    anchor.insertAdjacentHTML('beforeend',list.text);
+    anchor.insertAdjacentHTML('beforeend', value.text);
   });
   ul.appendChild(frag);
 });
