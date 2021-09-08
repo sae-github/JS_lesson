418675c4723b2db8387d272d8b0e17a5f2d5da83
sae-github <miyasae1126@gmail.com>
2021-09-08 19:52:53 +0900

    feat: Add a process to add a list to the DOM.

---
+++ lesson9/index.js
@@ -0,0 +1,48 @@
+const ul = document.getElementById('js-ul');
+
+const listData = [
+  { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
+  { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" }
+];
+
+function addLoading() {
+  const loading = document.createElement('img');
+  loading.src = "./loading-circle.gif";
+  loading.id = "loading";
+  ul.appendChild(loading);
+}
+
+function removeLoading() {
+  const loading = document.getElementById('loading');
+  loading.remove();
+}
+
+function getData() {
+  addLoading();
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(listData);
+    }, 3000);
+  });
+}
+
+async function createLists() {
+  const responseData = await getData();
+  removeLoading();
+  const frag = document.createDocumentFragment();
+  responseData.forEach((data) => {
+    const li = document.createElement('li');
+    const anchor = document.createElement('a');
+    const img = document.createElement('img');
+
+    anchor.href = `/${data.to}`;
+    img.src = data.img;
+    img.alt = data.alt;
+
+    frag.appendChild(li).appendChild(anchor).appendChild(img);
+    anchor.insertAdjacentHTML('beforeend', data.text);
+  });
+  ul.appendChild(frag);
+}
+
+createLists();
