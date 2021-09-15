cf29888bf995c6633f372c9fe2542a21d95b5c6f
sae-github <miyasae1126@gmail.com>
2021-09-15 22:58:33 +0900

    refactor:Change the try block Changed behavior when catching.

--- lesson10/index.js
+++ lesson10/index.js
@@ -1,5 +1,6 @@
 const ul = document.getElementById('js-ul');
 
+
 const listData = [
   { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
   { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" }
@@ -26,30 +27,33 @@ function getData() {
   });
 }
 
-async function createLists() {
+async function fetchData() {
   try {
-    const responseData = await getData();
-    const frag = document.createDocumentFragment();
-    responseData.forEach((data) => {
-      const li = document.createElement('li');
-      const anchor = document.createElement('a');
-      const img = document.createElement('img');
-
-      anchor.href = `/${data.to}`;
-      img.src = data.img;
-      img.alt = data.alt;
-
-      frag.appendChild(li).appendChild(anchor).appendChild(img);
-      anchor.insertAdjacentHTML('beforeend', data.text);
-    });
-    ul.appendChild(frag);
-  } catch (err) {
-    console.error(err);
-  }
-  finally {
+    return await getData();
+  } catch (e) {
+    console.error(e);
+    ul.textContent = "エラーが発生しました！";
+  } finally {
     removeLoading();
   }
 }
 
-createLists();
+async function createLists() {
+  const responseData = await fetchData();
+  const frag = document.createDocumentFragment();
+  responseData.forEach((data) => {
+    const li = document.createElement('li');
+    const anchor = document.createElement('a');
+    const img = document.createElement('img');
 
+    anchor.href = `/${data.to}`;
+    img.src = data.img;
+    img.alt = data.alt;
+
+    frag.appendChild(li).appendChild(anchor).appendChild(img);
+    anchor.insertAdjacentHTML('beforeend', data.text);
+  });
+  ul.appendChild(frag);
+}
+
+createLists();
