0f3876f4df93b82a3b21c5e336fa6ad5b6a35b29
sae-github <miyasae1126@gmail.com>
2021-09-23 16:17:14 +0900

    feat:Get json data and add it to the DOM

---
+++ lesson11/index.js
@@ -0,0 +1,55 @@
+const ul = document.getElementById('js-ul');
+
+function addLoading() {
+  const loading = document.createElement('img');
+  loading.src = "./loading-circle.gif";
+  loading.id = "loading";
+  ul.appendChild(loading);
+}
+
+function removeLoading() {
+  const loading = document.getElementById('loading');
+  loading.remove();
+}
+
+async function getData() {
+  addLoading();
+  try {
+    const response = await fetch("https://jsondata.okiba.me/v1/json/eGNos210920102817");
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    } else {
+      return response.json();
+    }
+  } catch (e) {
+    const div = document.getElementById('js-wrapper');
+    div.textContent = "エラーが発生しました！";
+    console.error(e);
+  } 
+}
+
+
+async function createLists() {
+  const responseData = await getData();
+  if (responseData !== undefined) {
+    removeLoading();
+    const frag = document.createDocumentFragment();
+    responseData.data.forEach((data) => {
+      const li = document.createElement('li');
+      const anchor = document.createElement('a');
+      const img = document.createElement('img');
+
+      anchor.href = `/${data.to}`;
+      img.src = data.img;
+      img.alt = data.alt;
+
+      frag.appendChild(li).appendChild(anchor).appendChild(img);
+      anchor.insertAdjacentHTML('beforeend', data.text);
+    });
+    ul.appendChild(frag);
+  }
+}
+
+createLists();
+
+
