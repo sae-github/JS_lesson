9542697942db218ad02b293d19ae868ed4b8e093
sae-github <miyasae1126@gmail.com>
2021-09-27 21:11:00 +0900

    refactor: Add tryCreate function.

--- lesson11/index.js
+++ lesson11/index.js
@@ -9,20 +9,30 @@ function addLoading() {
   ul.appendChild(loading);
 }
 
+
 function removeLoading() {
   const loading = document.getElementById('loading');
   loading.remove();
 }
 
-async function getData() {
+async function fetchData() {
   addLoading();
+  const response = await fetch("https://jsondata.okiba.me/v1/json/eGNos210920102817");
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  } else {
+    const json = await response.json();
+    return json;
+  }
+}
+
+async function tryCreate() {
   try {
-    const response = await fetch("https://jsondata.okiba.me/v1/json/eGNos210920102817");
-    if (!response.ok) {
-      throw new Error(response.statusText);
+    const responseData = await fetchData();
+    if (responseData.data) {
+      createLists(responseData);
     } else {
-      const json = await response.json();
-      return json;
+      div.textContent = "適切なデータが見つかりませんでした";
     }
   } catch (e) {
     div.textContent = "サーバーエラーが発生しました";
@@ -30,27 +40,23 @@ async function getData() {
   }
 }
 
-async function createLists() {
-  const responseData = await getData();
-  if (responseData.data) {
-    removeLoading();
-    const frag = document.createDocumentFragment();
-    responseData.data.forEach(data => {
-      const li = document.createElement('li');
-      const anchor = document.createElement('a');
-      const img = document.createElement('img');
 
-      anchor.href = `/${data.to}`;
-      img.src = data.img;
-      img.alt = data.alt;
+function createLists(responseData) {
+  removeLoading();
+  const frag = document.createDocumentFragment();
+  responseData.data.forEach(data => {
+    const li = document.createElement('li');
+    const anchor = document.createElement('a');
+    const img = document.createElement('img');
 
-      frag.appendChild(li).appendChild(anchor).appendChild(img);
-      anchor.insertAdjacentHTML('beforeend', data.text);
-    });
-    ul.appendChild(frag);
-  } else {
-    div.textContent = "適切なデータが見つかりませんでした";
-  }
+    anchor.href = `/${data.to}`;
+    img.src = data.img;
+    img.alt = data.alt;
+
+    frag.appendChild(li).appendChild(anchor).appendChild(img);
+    anchor.insertAdjacentHTML('beforeend', data.text);
+  });
+  ul.appendChild(frag);
 }
 
-createLists();
+tryCreate();
