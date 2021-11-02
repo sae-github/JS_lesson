9aacbcaec47b733cff4e0d8cfa2f8b0d67d1b71c
sae-github <miyasae1126@gmail.com>
2021-11-02 18:28:39 +0900

    feature: News UI
    Display data retrieved from the API by category
    
    Switching tabs

---
+++ Lesson16/main.js
@@ -0,0 +1,87 @@
+const tabContentList = document.getElementById("js-tab-content");
+const imgWrapper = document.getElementById("js-tab-img");
+const tabs = document.getElementsByClassName("tab-menu__item");
+
+
+async function getJsonOrError() {
+  const response = await fetch("./data.json");
+  const json = await fetchErrorHandling(response);
+  return json;
+}
+
+async function fetchErrorHandling(response) {
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error("サーバーエラーが発生しました");
+  }
+}
+
+async function tryGetData() {
+  try {
+    const data = await getJsonOrError();
+    return data;
+  } catch (error) {
+    tabContentList.replaceWith(error);
+  }
+}
+
+async function createElement({ article }) {
+  const articleLength = 4;
+  const frag = document.createDocumentFragment();
+  for (let i = 0; i < articleLength; i++) {
+    const li = document.createElement("li");
+    const anchor = document.createElement("a");
+    anchor.href = "#";
+    anchor.insertAdjacentHTML("beforeend", article[i].title);
+    frag.appendChild(li).appendChild(anchor);
+  }
+  tabContentList.appendChild(frag);
+}
+
+function addImage({ image }) {
+  const img = document.createElement("img");
+  img.src = image;
+  imgWrapper.appendChild(img);
+}
+
+
+async function createTabContent(index) {
+  const data = await tryGetData();
+  if (data) {
+    createElement(data[index]);
+    addImage(data[index]);
+  }
+}
+
+
+async function init() {
+  const data = await tryGetData();
+  if (data) {
+    const hasSelectData = data.filter((value) => {
+      return value.select === true;
+    });
+    const tab = document.getElementById(hasSelectData[0].category);
+    tab.click();
+  }
+}
+
+init();
+
+
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", (e) => {
+    const targetIndex = i;
+    const hasActiveElement = document.getElementsByClassName("active")[0];
+
+    if (hasActiveElement) {
+      hasActiveElement.classList.remove("active");
+    }
+    e.target.classList.add("active");
+
+    tabContentList.textContent = "";
+    imgWrapper.textContent = "";
+
+    createTabContent(targetIndex);
+  });
+}
