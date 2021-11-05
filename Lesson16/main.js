eb5ab04d926a04e3f9299aadea3e13608ea64b4d
sae-github <miyasae1126@gmail.com>
2021-11-05 22:19:46 +0900

    refactor:Fixed the way APIs are defined and loaded.

--- Lesson16/main.js
+++ Lesson16/main.js
@@ -1,3 +1,9 @@
+const API = {
+  news: "https://jsondata.okiba.me/v1/json/p4k7o211104194632",
+  book: "https://jsondata.okiba.me/v1/json/C2haH211104194737",
+  travel: "https://jsondata.okiba.me/v1/json/vbUXI211104194810",
+  economy: "https://jsondata.okiba.me/v1/json/RSWyy211104194854"
+};
 const tabMenuList = document.getElementById("js-tab-menu__list");
 
 function addLoading() {
@@ -37,13 +43,8 @@ async function getJsonOrError(url) {
 }
 
 async function getArrayFetchData() {
-  const newsApi = "https://jsondata.okiba.me/v1/json/p4k7o211104194632";
-  const bookApi = "https://jsondata.okiba.me/v1/json/C2haH211104194737";
-  const travelApi = "https://jsondata.okiba.me/v1/json/vbUXI211104194810";
-  const economyApi = "https://jsondata.okiba.me/v1/json/RSWyy211104194854";
-  const apiResources = [newsApi, bookApi, travelApi, economyApi];
   try {
-    const data = await Promise.all(apiResources.map(getJsonOrError));
+    const data = await Promise.all(Object.values(API).map(getJsonOrError));
     return data.filter((value) => value !== undefined);
   } catch (e) {
     addErrorMessage(e);
@@ -110,14 +111,8 @@ function addImage({ image }) {
 }
 
 async function createClickedTabContent(target) {
-  const apiResources = {
-    news: "https://jsondata.okiba.me/v1/json/p4k7o211104194632",
-    book: "https://jsondata.okiba.me/v1/json/C2haH211104194737",
-    travel: "https://jsondata.okiba.me/v1/json/vbUXI211104194810",
-    economy: "https://jsondata.okiba.me/v1/json/RSWyy211104194854"
-  };
   const targetId = await target.id;
-  const json = await tryGetData(apiResources[targetId]);
+  const json = await tryGetData(API[targetId]);
   createArticleElements(json);
   addImage(json);
 }
