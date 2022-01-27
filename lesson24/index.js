9cf36e4f691bb64dfcb89d46f2e8f2afe918b1ba
sae-github <miyasae1126@gmail.com>
2022-01-27 19:18:59 +0900

    refactor: Delete unnecessary IIFEs.

--- lesson24/index.js
+++ lesson24/index.js
@@ -1,25 +1,22 @@
 const modalContent = document.getElementById("js-modal-content");
 const submitButton = document.getElementById("js-submit-button");
 const checkBox = document.querySelector(".js-check-box");
+const overLay = document.getElementById("js-overlay");
+const closeButton = document.getElementById("js-modal-close");
+const rules = document.getElementById("js-rules");
+const body = document.querySelector("body");
 
-(function () {
-  const overLay = document.getElementById("js-overlay");
-  const closeButton = document.getElementById("js-modal-close");
-  const rules = document.getElementById("js-rules");
-  const body = document.querySelector("body");
+const closeModal = () => {
+  body.classList.remove("modal-open");
+};
 
-  const closeModal = () => {
-    body.classList.remove("modal-open");
-  };
+const openModal = () => {
+  body.classList.add("modal-open");
+};
 
-  const openModal = () => {
-    body.classList.add("modal-open");
-  };
-
-  rules.addEventListener("click", openModal);
-  closeButton.addEventListener("click", closeModal);
-  overLay.addEventListener("click", closeModal);
-})();
+rules.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
+overLay.addEventListener("click", closeModal);
 
 const scrollInModalContent = new IntersectionObserver(
   ([entry]) => {
