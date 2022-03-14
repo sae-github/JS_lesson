ba9e2ae4023911951f03964bd46598bbbc2fe335
sae-github <miyasae1126@gmail.com>
2022-03-14 20:54:59 +0900

    refactor: switching classes and changing the way attribute values are switched

--- lesson30/src/js/slideMenu.js
+++ lesson30/src/js/slideMenu.js
@@ -3,21 +3,10 @@ const overLay = document.getElementById("js-overlay");
 const slideMenu = document.getElementById("js-global-menu");
 const toggleButtonInSlideMenu = document.getElementById("js-slide-open-button");
 
-const openSlideMenu = () => {
-  slideMenu.setAttribute("aria-hidden", false);
-  body.classList.add("slideMenu-open");
+const toggleSlideMenu = () => {
+  slideMenu.toggleAttribute("aria-hidden");
+  body.classList.toggle("slideMenu-open");
 }
-
-const closeSlideMenu = () => {
-  slideMenu.setAttribute("aria-hidden", true);
-  body.classList.remove("slideMenu-open");
-}
-
-const isOpenSlideMenu = () => {
-  return body.classList.contains("slideMenu-open")
-}
-
-const toggleSlideMenu = () => isOpenSlideMenu() ? CloseSlideMenu() : openSlideMenu();
 
 toggleButtonInSlideMenu.addEventListener("click", toggleSlideMenu);
-overLay.addEventListener("click", closeSlideMenu);
+overLay.addEventListener("click", toggleSlideMenu);
