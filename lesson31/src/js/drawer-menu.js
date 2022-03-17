08a38f3df0e516a5f4afaef53c3c2352c21943a2
sae-github <miyasae1126@gmail.com>
2022-03-17 18:10:06 +0900

    feat: drawer menu add optional features

--- lesson31/src/js/drawer-menu.js
+++ lesson31/src/js/drawer-menu.js
@@ -1,5 +1,4 @@
 const body = document.body;
-const overLay = document.getElementById("js-drawer-overlay");
 const drawerMenu = document.getElementById("js-drawer-menu");
 const toggleButtonInDrawerMenu = document.getElementById("js-drawer-open-button");
 
@@ -8,5 +7,47 @@ const toggleDrawerMenu = () => {
   body.classList.toggle("drawer-menu-open");
 }
 
+const createOverLay = () => {
+  const overLay = document.createElement("div");
+  overLay.id = "js-drawer-overlay";
+  overLay.classList.add("drawer-overlay");
+  return overLay;
+}
+
+const renderOverLay = () => {
+  const overLay = createOverLay();
+  overLay.addEventListener("click", toggleDrawerMenu);
+  body.appendChild(overLay);
+}
+
+const switchDrawerDirect = (option) => {
+  switch (option) {
+    case "left":
+      {
+        drawerMenu.classList.add("drawer--left");
+        break;
+      }
+    case "right":
+      {
+        drawerMenu.classList.add("drawer--right");
+        break;
+      }
+    default:
+      drawerMenu.classList.add("drawer--left");
+  }
+}
+
+const settingDrawerMenu = (options) => {
+  const drawerOptions = {
+    direct: "left",
+    overLay: true,
+    duration: 0.3,
+    ...options
+  }
+  drawerOptions.overLay && renderOverLay();
+  switchDrawerDirect(drawerOptions.direct);
+  drawerMenu.style.transitionDuration = `${drawerOptions.duration}s`;
+}
+
+settingDrawerMenu({ direct: "right" });
 toggleButtonInDrawerMenu.addEventListener("click", toggleDrawerMenu);
-overLay.addEventListener("click", toggleDrawerMenu);
