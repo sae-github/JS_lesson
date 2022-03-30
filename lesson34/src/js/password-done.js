7b1be0976555d28ff4a726b47478b3c3a6c04ab5
sae-github <miyasae1126@gmail.com>
2022-03-30 16:49:05 +0900

    feat: lesson34

---
+++ lesson34/src/js/password-done.js
@@ -0,0 +1,12 @@
+const checkUrlParams = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const token = localStorage.getItem("forgotPasswordDoneToken");
+  if (token && urlParams.get("token") === token) {
+    return;
+  }
+  window.location.href = "../notauthorize.html";
+}
+
+checkUrlParams();
+localStorage.removeItem("forgotPasswordDoneToken");
+setTimeout(() => window.location.href = "../login.html", 3000);
