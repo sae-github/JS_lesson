1e0e9da5caee2a3848f04a993fb5b4a5b1f42a94
sae-github <miyasae1126@gmail.com>
2022-04-08 13:25:25 +0900

    feat: lesson35

---
+++ lesson35/src/js/archive.js
@@ -0,0 +1,140 @@
+const archiveWrapper = document.getElementById("js-archive-wrapper");
+let defaultArticleList;
+
+const createElementWithClassName = (type, name) => {
+  const element = document.createElement(type);
+  element.className = name;
+  return element;
+}
+
+const renderLoading = (parent) => {
+  const loading = createElementWithClassName("div", "archive-loading");
+  const img = document.createElement("img");
+  loading.id = "js-loading";
+  img.src = "./img/loading-circle.gif";
+  parent.appendChild(loading).appendChild(img);
+}
+
+const removeLoading = () => document.getElementById("js-loading").remove();
+
+const fetchErrorHandling = async (response) => {
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error("サーバーエラーが発生しました");
+  }
+}
+
+const renderErrorMessage = (error, parent) => {
+  const errorMessage = createElementWithClassName("p", "error-message");
+  errorMessage.textContent = error;
+  parent.appendChild(errorMessage);
+}
+
+const getJsonOrError = async (url) => {
+  const response = await fetch(url);
+  const json = await fetchErrorHandling(response);
+  return json;
+}
+
+const getData = async (parent, resource) => {
+  renderLoading(parent);
+  try {
+    return await getJsonOrError(resource);
+  } catch (e) {
+    renderErrorMessage(e, parent);
+  } finally {
+    removeLoading();
+  }
+}
+
+const renderCategorySelect = (data) => {
+  const selectWrapper = createElementWithClassName("div", "archive__select");
+  const select = document.createElement("select");
+  select.addEventListener("change", (e) => renderSelectedCategoryList(e.target, data));
+  archiveWrapper
+    .appendChild(selectWrapper)
+    .appendChild(select)
+    .appendChild(createSelectOptions(data));
+}
+
+const createSelectOptions = (data) => {
+  const frag = document.createDocumentFragment();
+  const defaultOption = document.createElement("option");
+  defaultOption.textContent = "未選択";
+  defaultOption.value = "default";
+  frag.appendChild(defaultOption);
+  data.forEach(({ category }) => {
+    const option = document.createElement("option");
+    option.value = category;
+    option.textContent = category;
+    frag.appendChild(option);
+  });
+  return frag;
+}
+
+const renderSelectedCategoryList = (target, data) => {
+  const archiveList = document.getElementById("js-archive-list");
+  if (target.value === "default") {
+    archiveList.replaceWith(defaultArticleList.cloneNode(true));
+    return;
+  }
+  archiveList.textContent = "";
+  const selectedCategoryData = data.find(({ category }) => category === target.value);
+  archiveList.appendChild(createArticleItem(selectedCategoryData));
+}
+
+const renderArchiveList = (data) => {
+  const archiveList = createElementWithClassName("ul", "archive__list");
+  const frag = document.createDocumentFragment();
+  archiveList.id = "js-archive-list";
+  data.forEach((d) => frag.appendChild(createArticleItem(d)));
+  archiveWrapper.appendChild(archiveList).appendChild(frag);
+}
+
+const createArticleItem = (data) => {
+  const frag = document.createDocumentFragment();
+  data.article.forEach((article) => {
+    const archiveItem = createElementWithClassName("li", "archive__item");
+    const archiveLink = createElementWithClassName("a", "archive__item-link");
+    const archiveWrapper = createElementWithClassName("div", "archive__item-text");
+    const title = createElementWithClassName("p", "archive__item-title");
+    const date = createElementWithClassName("p", "archive__item-date");
+    const label = createElementWithClassName("span", "archive__item-label");
+    const time = document.createElement("time");
+    time.textContent = article.date;
+    label.textContent = data.category;
+    title.textContent = article.title;
+    archiveLink.href = `./article.html?id=${article.id}`;
+
+    archiveWrapper.appendChild(label).after(title);
+    archiveWrapper.appendChild(date).appendChild(time);
+    archiveLink.appendChild(createThumbnail(article));
+    frag
+      .appendChild(archiveItem)
+      .appendChild(archiveLink)
+      .appendChild(archiveWrapper)
+  });
+  return frag;
+}
+
+const createThumbnail = ({ thumbnail }) => {
+  const thumbnailWrapper = createElementWithClassName("div", "archive__item-thumbnail");
+  const img = document.createElement("img");
+  img.src = thumbnail;
+  thumbnail === "" && (img.src = "./img/no-image.jpeg");
+  thumbnailWrapper.appendChild(img);
+  return thumbnailWrapper;
+}
+
+const init = async () => {
+  const url = "https://mocki.io/v1/9c2bbf94-fbb8-40f1-90c7-cc1870ac6cfa";
+  const { data } = await getData(archiveWrapper, url);
+  if (data) {
+    renderCategorySelect(data);
+    renderArchiveList(data);
+    defaultArticleList = document.getElementById("js-archive-list").cloneNode(true);
+  }
+}
+
+init();
---
+++ lesson35/src/js/tabMenu.js
@@ -0,0 +1,291 @@
+import { format, differenceInCalendarDays } from "date-fns";
+const tabMenu = document.getElementById("js-tab-menu");
+
+const API = {
+  news: "https://mocki.io/v1/8f25b166-c2e4-460e-b10d-43802067b457",
+  book: "https://mocki.io/v1/50075330-2da2-45e0-8c02-9d55fa12e997",
+  economy: "https://mocki.io/v1/9a0a7e47-bc46-46d7-80a6-f7daa1580275",
+  travel: "https://mocki.io/v1/7955e7c4-56fd-4ee6-8c8e-bda45aa38783"
+};
+
+const articleAPI = {
+  "8b5ae244-cddb-4b94-a5cd-b1ca4201c945":
+    "https://mocki.io/v1/47c8a0bf-25cc-469f-8bf3-5de1d50eb260",
+  "8b5ae244-cddb-4b94-a5cd-b1ca4201c946":
+    "https://mocki.io/v1/eaada9c5-44ef-46a6-a795-ba9dbc7e8de8",
+  "7cb39430-d000-4b2a-8bbd-29a6ff6c7694":
+    "https://mocki.io/v1/c1fb9a87-70c7-4fed-89c1-894a3d2712ae",
+  "7cb39430-d000-4b2a-8bbd-29a6ff6c7697":
+    "https://mocki.io/v1/c1d113bb-2fee-4558-ac89-75dc91ab874f",
+  "4c42eb6a-dc41-4138-ac0a-49062e4a55e3":
+    "https://mocki.io/v1/74b405cf-62e0-4e91-85f7-af7428031569",
+  "9d50f525-b5a3-469d-ad28-0bbaa3f38fcc":
+    "https://mocki.io/v1/f11e19f2-5bd2-478f-9fcc-5a79d4591571",
+  "9d50f525-b5a3-469d-ad28-0bbaa3f38fce":
+    "https://mocki.io/v1/dccb7735-bede-4915-96cc-7c02bfe9e454"
+};
+
+const createElementWithClassName = (type, className) => {
+  const element = document.createElement(type);
+  element.className = className;
+  return element;
+};
+
+const createLoading = () => {
+  const loading = createElementWithClassName("img", "loading");
+  loading.id = "js-loading";
+  loading.src = "./img/loading-circle.gif";
+  return loading;
+};
+
+const removeLoading = () => document.getElementById("js-loading").remove();
+
+const createErrorMessage = (error) => {
+  const errorMessage = createElementWithClassName("p", "error-message");
+  errorMessage.textContent = error;
+  return errorMessage;
+};
+
+const fetchErrorHandling = async (response) => {
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error("サーバーエラーが発生しました");
+  }
+};
+
+const getJsonOrError = async (url) => {
+  const response = await fetch(url);
+  const json = await fetchErrorHandling(response);
+  return json;
+};
+
+const getArticleData = async () => {
+  const newsSection = document.getElementById("js-news");
+  try {
+    const data = await Promise.all(Object.values(API).map(getJsonOrError));
+    return data.filter((value) => value !== undefined);
+  } catch (e) {
+    newsSection.appendChild(createErrorMessage(e));
+  }
+}
+
+const createTabMenu = (data) => {
+  const tabMenuFrag = document.createDocumentFragment();
+  for (let i = 0; i < data.length; i++) {
+    const tabMenuItem = createElementWithClassName("li", "news-tab__menu-item");
+    tabMenuItem.id = data[i].category;
+    tabMenuItem.textContent = data[i].category;
+    tabMenuFrag.appendChild(tabMenuItem);
+  }
+  return tabMenuFrag;
+}
+
+const createTabContent = () => {
+  const tabContent = createElementWithClassName("div", "news-tab__content");
+  const imageWrapper = createElementWithClassName("div", "news-tab__img-wrapper");
+  const tabContentList = document.createElement("ul");
+
+  tabContent.id = "js-tab-content";
+  tabContentList.id = "js-tab-list";
+  imageWrapper.id = "js-tab-img";
+
+  tabContent.appendChild(tabContentList);
+  tabContent.appendChild(imageWrapper);
+  return tabContent;
+}
+
+const configUIfromFetchData = async () => {
+  const data = await getArticleData();
+  if (data) {
+    tabMenu.after(createTabContent());
+    tabMenu.appendChild(createTabMenu(data));
+    const hasSelectData = data.find((value) => value.select === true);
+    const tab = document.getElementById(hasSelectData.category);
+    tab.classList.add("tab-select");
+    document.getElementById("js-tab-list").append(createArticleElements(hasSelectData));
+    document.getElementById("js-tab-img").append(createCategoryImg(hasSelectData));
+  }
+}
+
+const isSpecifiedPeriod = (date) => {
+  const newArrivalDays = 3;
+  const today = format(new Date(), "yyyy,MM,dd");
+  const articleDate = format(new Date(date), "yyyy,MM,dd");
+  const periodOfDays = differenceInCalendarDays(
+    new Date(today),
+    new Date(articleDate)
+  );
+  const result = periodOfDays <= newArrivalDays;
+  return result;
+}
+
+const createNewLabel = () => {
+  const newLabel = createElementWithClassName("p", "new-label");
+  newLabel.textContent = "New";
+  return newLabel;
+}
+
+const hasComment = (commentLength) => commentLength > 0;
+
+const createCommentLength = (commentLength) => {
+  const commentWrapper = createElementWithClassName("span", "comment-length");
+  const commentIcon = createElementWithClassName("img", "comment-icon");
+  commentIcon.src = "./img/comment-icon.svg";
+  commentWrapper.appendChild(commentIcon);
+  commentWrapper.insertAdjacentHTML("beforeend", commentLength);
+  setClickEventInCommentIcon(commentWrapper);
+  return commentWrapper;
+}
+
+const createArticleElements = ({ article }) => {
+  const articleFrag = document.createDocumentFragment();
+  for (let i = 0; i < article.length; i++) {
+    const metaWrapper = createElementWithClassName("div", "meta-wrapper");
+    const commentLength = article[i].comments.length;
+    isSpecifiedPeriod(article[i].date) && metaWrapper.appendChild(createNewLabel());
+    hasComment(commentLength) && metaWrapper.appendChild(createCommentLength(commentLength));
+
+    const li = document.createElement("li");
+    li.id = article[i].id;
+    const anchor = document.createElement("a");
+    anchor.href = `./article.html?id=${article[i].id}`;
+    anchor.insertAdjacentHTML("beforeend", article[i].title);
+    articleFrag.appendChild(li).appendChild(anchor).after(metaWrapper);
+  }
+  return articleFrag;
+}
+
+const createCategoryImg = ({ image }) => {
+  const categoryImg = document.createElement("img");
+  categoryImg.src = image;
+  return categoryImg;
+}
+
+const createClickedTabContent = async (target) => {
+  const tabContent = document.getElementById("js-tab-content");
+  const targetId = await target.id;
+  const json = await tryGetData(tabContent, API[targetId]);
+  document.getElementById("js-tab-list").appendChild(createArticleElements(json));
+  document.getElementById("js-tab-img").appendChild(createCategoryImg(json));
+  target.style.pointerEvents = "auto";
+}
+
+const tryGetData = async (parent, resource) => {
+  parent.appendChild(createLoading());
+  try {
+    return await getJsonOrError(resource);
+  } catch (e) {
+    parent.appendChild(createErrorMessage(e));
+  } finally {
+    removeLoading();
+  }
+}
+
+const createModal = () => {
+  const modal = createElementWithClassName("div", "modal");
+  const modalInner = createElementWithClassName("div", "modal__inner");
+  modal.id = "js-modal";
+  modalInner.id = "js-modal-inner";
+  modal.appendChild(modalInner);
+  return modal;
+}
+
+const createModalCloseIcon = () => {
+  const modalCloseIconWrapper = createElementWithClassName("div", "modal__close-wrapper");
+  const closeIcon = document.createElement("img");
+  closeIcon.src = "./img/cross-icon.svg";
+  setClickEventForModalClose(closeIcon);
+  modalCloseIconWrapper.appendChild(closeIcon);
+  return modalCloseIconWrapper;
+}
+
+const createOverLay = () => {
+  const overLay = createElementWithClassName("div", "overlay");
+  overLay.id = "js-over-lay";
+  setClickEventForModalClose(overLay);
+  return overLay;
+}
+
+const addModal = () => {
+  const newsSection = document.querySelector(".news");
+  newsSection.appendChild(createModal()).appendChild(createModalCloseIcon());
+}
+
+const addOverLay = () => {
+  document.getElementById("js-news").appendChild(createOverLay());
+}
+
+const setClickEventForModalClose = (target) => {
+  target.addEventListener("click", () => {
+    document.getElementById("js-modal-inner").textContent = "";
+    document.querySelector("body").classList.remove("modal-open");
+  });
+}
+
+const openModalAndOverLay = () => {
+  document.querySelector("body").classList.add("modal-open");
+
+}
+
+const setClickEventInCommentIcon = (target) => {
+  target.addEventListener("click", async (e) => {
+    openModalAndOverLay();
+    const toAppendElement = document.getElementById("js-modal-inner");
+    const targetParentId = e.currentTarget.closest("li").id;
+    const commentData = await getComment(targetParentId, toAppendElement);
+    toAppendElement.appendChild(createModalContent(commentData));
+  });
+}
+
+const getClickedArticleData = async (resource, parent) => {
+  return await tryGetData(parent, articleAPI[resource]);
+}
+
+const getComment = async (targetId, parent) => {
+  const responseData = await getClickedArticleData(targetId, parent);
+  const { comments } = responseData;
+  return comments;
+}
+
+const createModalContent = (commentData) => {
+  const commentContentFrag = document.createDocumentFragment();
+  for (const { name, detail, icon } of commentData) {
+    const modalItem = createElementWithClassName("div", "modal-item");
+    const detailWrapper = createElementWithClassName("div", "modal__detail-wrapper");
+    const imgWrapper = createElementWithClassName("div", "modal__img-wrapper");
+    const userName = createElementWithClassName("p", "user-name");
+    const image = createElementWithClassName("img", "user-icon");
+    const comment = createElementWithClassName("p", "user-comment");
+    userName.textContent = name;
+    comment.textContent = detail;
+    image.src = icon;
+    detailWrapper.appendChild(userName);
+    detailWrapper.appendChild(comment);
+    imgWrapper.appendChild(image);
+    commentContentFrag.appendChild(modalItem).appendChild(detailWrapper);
+    modalItem.insertBefore(imgWrapper, detailWrapper);
+  }
+  return commentContentFrag;
+}
+
+configUIfromFetchData();
+addModal();
+addOverLay();
+
+tabMenu.addEventListener("click", (e) => {
+  const hasActiveClassElement = document.querySelector(".tab-select");
+  if (hasActiveClassElement && e.currentTarget !== e.target) {
+    hasActiveClassElement.classList.remove("tab-select");
+    e.target.classList.add("tab-select");
+
+    const imgWrapper = document.getElementById("js-tab-img");
+    const tabContentList = document.getElementById("js-tab-list");
+
+    tabContentList.textContent = "";
+    imgWrapper.textContent = "";
+    e.target.style.pointerEvents = "none";
+    createClickedTabContent(e.target);
+  }
+});
+
