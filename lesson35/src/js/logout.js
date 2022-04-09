743e8e003494b41265e6f36a0696c85442bee994
sae-github <miyasae1126@gmail.com>
2022-04-09 16:11:23 +0900

    feat: lesson35

--- lesson35/src/js/logout.js
+++ lesson35/src/js/logout.js
@@ -3,5 +3,5 @@ const logout = document.getElementById("js-logout");
 logout.addEventListener("click", (e) => {
   e.preventDefault();
   localStorage.removeItem("token");
-  window.location.href = "./login.html";
+  window.location.href = "./notloginuserpage.html";
 });
